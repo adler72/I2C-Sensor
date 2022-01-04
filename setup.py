7e16404da2a2f75471581c718e71760bd544d790
adler72 <53665786+adler72@users.noreply.github.com>
2022-01-04 18:51:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='I2CSensor',
       version='0.0.1',
       description='CraftBeerPi Plugin',
-      author='',
-      author_email='',
-      url='',
+      author='Marc Adler',
+      author_email='aeda@gmx.de',
+      url='https://github.com/adler72/I2C-Sensor',
+      license='GPLv3',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
